62a5876d3c8f21d7ad3e1352ded7065bbfed482f
Reeve Fernandes <ferna312@msu.edu>
2024-05-01 14:09:19 -0400

    api config

--- app/page.tsx
+++ app/page.tsx
@@ -12,19 +12,29 @@ export default function Home() {
 	};
 
 	return (
-		<div>
-			<div>
-				<h1>Enter Your Name</h1>
+		<div className="min-h-screen flex items-center justify-center bg-gray-100">
+			<div className="p-4 shadow-md bg-white rounded-md">
+				<h1 className="text-2xl font-semibold mb-4 text-black">
+					Enter Your Name
+				</h1>
 			</div>
-			<form onSubmit={handleSubmit}>
+			<form
+				onSubmit={handleSubmit}
+				className="space-y-3"
+			>
 				<input
 					type="text"
 					placeholder="Type your name..."
 					value={name}
-					className="text-black"
+					className="w-full p-2 border border-gray-300 rounded text-black"
 					onChange={(e) => setName(e.target.value)}
 				/>
-				<button type="submit">Predict Data</button>
+				<button
+					type="submit"
+					className="w-full py-2 px-4 bg-blue-500 hover:bg-blue-600 text-white font-semibold rounded-lg shadow-md focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75"
+				>
+					Predict Data
+				</button>
 			</form>
 		</div>
 	);
--- app/prediction/[name]/page.tsx
+++ app/prediction/[name]/page.tsx
@@ -1,3 +1,43 @@
-export default function prediction({ params }: any) {
-	return <div> {params.name}</div>;
+//  Predict Age
+const getPredictedAge = async (name: string) => {
+	const res = await fetch(`https://api.agify.io?name=${name}`);
+	return res.json();
+};
+
+// Predict Gender
+const getPredictedGender = async (name: string) => {
+	const res = await fetch(`https://api.genderize.io?name=${name}`);
+	return res.json();
+};
+
+// Predict Country
+const getPredictedCountry = async (name: string) => {
+	const res = await fetch(`https://api.nationalize.io?name=${name}`);
+	return res.json();
+};
+
+interface Params {
+	params: { name: string };
+}
+
+export default async function prediction({ params }: Params) {
+	const ageData = getPredictedAge(params.name);
+	const genderData = getPredictedGender(params.name);
+	const countryData = getPredictedCountry(params.name);
+
+	const [age, gender, country] = await Promise.all([
+		ageData,
+		genderData,
+		countryData
+	]);
+	return (
+		<div>
+			<div>
+				<div> Personal Info</div>
+				<div> Age: {age?.age}</div>
+				<div> Gender: {gender?.gender}</div>
+				<div> Country: {country?.country?.[0]?.country_id}</div>
+			</div>
+		</div>
+	);
 }
