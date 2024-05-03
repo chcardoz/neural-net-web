aee3a1360bf6d3fe7b844b74dde5a9478eaa8ac0
Reeve Fernandes <ferna312@msu.edu>
2024-05-03 16:05:58 -0400

    Add references to Weights class

--- app/node.tsx
+++ app/node.tsx
@@ -1,10 +1,16 @@
 import { Component } from "react";
 
 export class Node extends Component {
+	constructor(props: any) {
+		super(props);
+		this.state = {
+			value: 0
+		};
+	}
 	render() {
 		return (
 			<div className="flex items-center justify-center rounded-full w-16 h-16 bg-gray-500">
-				<span className="text-white">Node</span>
+				<span className="text-white">{this.state.value}</span>
 			</div>
 		);
 	}
--- app/weight.tsx
+++ app/weight.tsx
@@ -1,8 +1,28 @@
 import { Component } from "react";
 
 export class Weight extends Component {
+	constructor(props: any) {
+		super(props);
+		this.state = {
+			node1: null,
+			node2: null
+		};
+	}
+
+	// Function to update the references to the connected nodes
+	setConnectedNodes = (node1: any, node2: any) => {
+		this.setState({ node1, node2 });
+	};
+
 	render() {
-		return <div>Weight</div>;
+		// Access node1 and node2 from state or props
+		const { node1: any, node2: any } = this.state;
+
+		return (
+			<div>
+				<h2>Weights Component</h2>
+			</div>
+		);
 	}
 }
 
