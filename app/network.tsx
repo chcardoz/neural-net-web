8f2b4c3544b063d774ac1c25e172030679b3457d
Reeve Fernandes <ferna312@msu.edu>
2024-05-03 15:46:21 -0400

    visual set up

--- app/network.tsx
+++ app/network.tsx
@@ -6,10 +6,13 @@ import { Layer } from "./layer";
 export class Network extends Component {
 	render() {
 		return (
-			<div>
-				<Layer />
-				network
-				<Layer />
+			<div className="flex">
+				<div className="w-1/2 bg-gray-200 p-4">
+					<Layer />
+				</div>
+				<div className="w-1/2 bg-blue-200 p-4">
+					<Layer />
+				</div>
 			</div>
 		);
 	}
