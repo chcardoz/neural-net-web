7cf00de2366fb6b2abfe8324e2fd3fb5fb7c806e
Reeve Fernandes <ferna312@msu.edu>
2024-05-20 14:54:31 -0400

    page changes

--- app/page.tsx
+++ app/page.tsx
@@ -4,54 +4,6 @@ import { useState, useEffect } from "react";
 import useASTAnalyzer from "@/lib/astanalyzer";
 
 /**
- * Represents a value.
- */
-class Value {
-	/**
-	 * The name of the value.
-	 * @type {string | null}
-	 */
-	name: string | null;
-
-	/**
-	 * The numeric value.
-	 * @type {number | null}
-	 */
-	value: number | null;
-
-	/**
-	 * The children values.
-	 * @type {Array<Value> | null}
-	 */
-	children: Array<Value> | null;
-
-	/**
-	 * The operation associated with the value.
-	 * @type {string}
-	 */
-	op: string;
-
-	/**
-	 * Constructs a new Value object.
-	 * @param {string | null} name - The name of the value.
-	 * @param {number} value - The numeric value.
-	 * @param {Array<Value>} children - The children values.
-	 * @param {string} op - The operation associated with the value.
-	 */
-	constructor(
-		name: string | null,
-		value: number,
-		children: Array<Value>,
-		op: string
-	) {
-		this.name = name;
-		this.value = value;
-		this.children = children;
-		this.op = op;
-	}
-}
-
-/**
  * Renders the Home component.
  *
  * @returns The rendered Home component.
