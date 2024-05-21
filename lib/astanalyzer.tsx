ceee6f8d737228c0ed2c8aa210405c751803d204
Reeve Fernandes <ferna312@msu.edu>
2024-05-21 16:48:47 -0400

    Moved things around: Moved Value class into it's own file

--- lib/astanalyzer.tsx
+++ lib/astanalyzer.tsx
@@ -9,54 +9,7 @@ import {
 	Literal,
 	Identifier
 } from "acorn";
-
-/**
- * Represents a value.
- */
-class Value {
-	/**
-	 * The name of the value.
-	 * @type {string | null}
-	 */
-	name: string | null;
-
-	/**
-	 * The numeric value.
-	 * @type {number | null}
-	 */
-	value: number | null;
-
-	/**
-	 * The children values.
-	 * @type {Array<Value> | null}
-	 */
-	children: Array<Value> | null;
-
-	/**
-	 * The operation associated with the value.
-	 * @type {string}
-	 */
-	op: string | null;
-
-	/**
-	 * Constructs a new Value object.
-	 * @param {string | null} name - The name of the value.
-	 * @param {number} value - The numeric value.
-	 * @param {Array<Value>} children - The children values.
-	 * @param {string} op - The operation associated with the value.
-	 */
-	constructor(
-		name: string | null,
-		value: number | null,
-		children: Array<Value> | null,
-		op: string | null
-	) {
-		this.name = name;
-		this.value = value;
-		this.children = children;
-		this.op = op;
-	}
-}
+import { Value } from "../lib/Value";
 
 type IdentifierMap = Set<Value>;
 type ASTAnalyzerReturnType = {
