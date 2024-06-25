a3cd6eeb1c552fb238ca15fcf1cfc5b9c231d795
Reeve Fernandes <ferna312@msu.edu>
2024-06-25 15:02:39 -0400

    front end set up

---
+++ app/newUI/page.tsx
@@ -0,0 +1,5 @@
+import Home from "../../lib/Home";
+
+export default function HomePage() {
+    return <Home />;
+}
--- lib/Home.tsx
+++ lib/Home.tsx
@@ -1,21 +1,9 @@
-const Home: React.FC = () => {
-    return (
-        <div className="flex flex-col h-screen bg-gradient-to-b from-sky-300 p-4">
-            <div className="p-2 flex-none">
-                <p>NEURAL NET VISUALIZER</p>
-            </div>
-            <div className="bg-white p-5 rounded-lg shadow-lg flex-auto">
-                <p>Your content goes here...</p>
-            </div>
+"use client";
 
-            <div
-                id="floating-div"
-                className="absolute bottom-4 bg-slate-50 border-dashed border-2 p-4 cursor-move"
-            >
-                Floating Div
-            </div>
-        </div>
-    );
+import Nav from "@/app/newUI/nav";
+
+const Home: React.FC = () => {
+    return <Nav />;
 };
 
 export default Home;
